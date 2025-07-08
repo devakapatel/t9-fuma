4c8bb99cea27a7e3edbf106c9c14efd70ec6a562
devakapatel <dev.akash.patel@gmail.com>
2025-07-08 11:03:00 +0530

    Update source.ts

--- lib/source.ts
+++ lib/source.ts
@@ -1,6 +1,8 @@
 import { about, sem6, sem7 } from '@/.source';
 import { loader } from 'fumadocs-core/source';
 
+export const runtime = 'edge';
+
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const aboutSource = loader({
   // it assigns a URL to your pages
