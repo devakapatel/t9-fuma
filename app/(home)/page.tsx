ffea11adde1b044dfcbffd34f185c53d4661a26a
devakapatel <85642873+devakapatel@users.noreply.github.com>
2025-07-24 11:29:49 +0530

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,9 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">T9 Alpha Testing</h1>
+      <h1 className="mb-4 text-2xl font-bold">T9 Beta Testing</h1>
       <p className="text-fd-muted-foreground">
         <Link
           href="/about"
@@ -17,7 +17,7 @@ export default function HomePage() {
           href="/sem-7"
           className="text-fd-foreground font-semibold underline"
         >
-          Semester  7
+          Semester 7
         </Link>
       </p>
     </main>
