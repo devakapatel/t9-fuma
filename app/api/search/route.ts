4982598344d752567b89952cf12df03b5885ab20
devakapatel <85642873+devakapatel@users.noreply.github.com>
2025-07-24 11:06:34 +0530

    Update route.ts

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,16 +1,16 @@
-import { aboutSource, sem6Source, sem7Source } from '@/lib/source';
-import { createSearchAPI } from 'fumadocs-core/search/server';
- 
-export const { GET } = createSearchAPI('advanced', {
+import { aboutSource, sem6Source, sem7Source } from "@/lib/source";
+import { createSearchAPI } from "fumadocs-core/search/server";
+
+export const { staticGET: GET } = createSearchAPI("advanced", {
   indexes: [
     ...aboutSource.getPages(),
     ...sem6Source.getPages(),
-    ...sem7Source.getPages()
+    ...sem7Source.getPages(),
   ].map((page) => ({
     title: page.data.title,
     description: page.data.description,
     url: page.url,
     id: page.url,
-    structuredData: page.data.structuredData
-  }))
+    structuredData: page.data.structuredData,
+  })),
 });
