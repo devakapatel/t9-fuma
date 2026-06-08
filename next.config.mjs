96a8f143842ceec3a97ad799f74c64e6b73b0632
devakapatel <dev.akash.patel@gmail.com>
2026-06-08 10:42:55 +0530

    Make it work for github

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const withMDX = createMDX();
 const config = {
   reactStrictMode: true,
   output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   images: {
     unoptimized: true,
   },
